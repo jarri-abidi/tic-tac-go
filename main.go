ebac5d758379f320101fc6ff215e680c54063be3
Jarri Abidi <jarri1337@gmail.com>
2020-03-29 16:00:19 +0500

    Run gofmt -s

--- main.go
+++ main.go
@@ -1,48 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"github.com/faiface/pixel"
-	"github.com/faiface/pixel/pixelgl"
 	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 	"image/color"
 	"math"
-	"fmt"
 )
 
 const (
-	stateO string = "O"
-	stateX string = "X"
+	stateO     string = "O"
+	stateX     string = "X"
 	stateEmpty string = ""
 )
 
 var (
-	zv = pixel.ZV
-	squareLength = float64(170)
-	crossLength = float64(90)
-	circleRadius = float64(50)
+	zv               = pixel.ZV
+	squareLength     = float64(170)
+	crossLength      = float64(90)
+	circleRadius     = float64(50)
 	drawingThickness = float64(10)
-	squareColor = colornames.Black
-	circleColor = colornames.Red
-	crossColor = colornames.Blue
-	lineOffset = squareLength/3
-	first = squareLength/2
-	second = (squareLength/2)*3
-	third = (squareLength/2)*5
-	coordinates = [][]vector {
-		{ v(first, first), v(first, second), v(first, third) },
-		{ v(second, first), v(second, second), v(second, third) },
-		{ v(third, first), v(third, second), v(third, third) },
+	squareColor      = colornames.Black
+	circleColor      = colornames.Red
+	crossColor       = colornames.Blue
+	lineOffset       = squareLength / 3
+	first            = squareLength / 2
+	second           = (squareLength / 2) * 3
+	third            = (squareLength / 2) * 5
+	coordinates      = [][]vector{
+		{v(first, first), v(first, second), v(first, third)},
+		{v(second, first), v(second, second), v(second, third)},
+		{v(third, first), v(third, second), v(third, third)},
 	}
-	offsets = [][]vector {
-		{ v(-lineOffset, -lineOffset), v(-lineOffset, 0), v(-lineOffset, lineOffset) },
-		{ v(0, -lineOffset), v(0, 0), v(0, lineOffset) },
-		{ v(lineOffset, -lineOffset), v(lineOffset, 0), v(lineOffset, lineOffset) },
+	offsets = [][]vector{
+		{v(-lineOffset, -lineOffset), v(-lineOffset, 0), v(-lineOffset, lineOffset)},
+		{v(0, -lineOffset), v(0, 0), v(0, lineOffset)},
+		{v(lineOffset, -lineOffset), v(lineOffset, 0), v(lineOffset, lineOffset)},
 	}
 	state = make(map[vector]string)
 )
 
-type vector = pixel.Vec 
+type vector = pixel.Vec
 
 func v(x, y float64) vector {
 	return pixel.V(x, y)
@@ -99,9 +99,9 @@ func run() {
 						lineColor = crossColor
 					} else {
 						lineColor = circleColor
-					} 
+					}
 					drawLine(imd, coordinates[x1][y1], coordinates[x2][y2], offsets[x1][y1], offsets[x2][y2], drawingThickness, lineColor)
-				} 
+				}
 			}
 		}
 		win.Update()
@@ -127,8 +127,8 @@ func drawSquare(imd *imdraw.IMDraw, length, x, y, thickness float64) {
 	imd.Push(v(x+length, y))
 	imd.Push(v(x+length, y+length))
 	imd.Rectangle(thickness)
-	for i:=0; i<3; i++ {
-		for j:=0; j<3; j++ {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
 			state[coordinates[i][j]] = stateEmpty
 		}
 	}
@@ -142,7 +142,7 @@ func drawO(imd *imdraw.IMDraw, c vector, radius, thickness float64, color color.
 
 func drawX(imd *imdraw.IMDraw, c vector, length, thickness float64, color color.Color) {
 	imd.Color = color
-	l := length/2
+	l := length / 2
 	imd.Push(v(c.X-l, c.Y-l), v(c.X+l, c.Y+l))
 	imd.Line(thickness)
 	imd.Push(v(c.X+l, c.Y-l), v(c.X-l, c.Y+l))
@@ -165,9 +165,9 @@ func getNearestSquare(click vector) vector {
 	minDistance := math.MaxFloat64
 	var nearestX float64
 	var nearestY float64
-	for i:=0; i<3; i++ {
-		for j:=0; j<3; j++ {
-			square := coordinates[i][j] 
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			square := coordinates[i][j]
 			result := click.Sub(square)
 			diff := math.Abs(result.X) + math.Abs(result.Y)
 			if minDistance > diff {
@@ -182,30 +182,44 @@ func getNearestSquare(click vector) vector {
 
 func checkWinner() (bool, string, vector, vector) {
 	// check horizontal win
-	if gameOver, winner := checkHorizontalWin(1, 2); gameOver {	return true, winner, v(0,2), v(2,2) }
-	if gameOver, winner := checkHorizontalWin(1, 1); gameOver { return true, winner, v(0,1), v(2,1) }
-	if gameOver, winner := checkHorizontalWin(1, 0); gameOver { return true, winner, v(0,0), v(2,0) }
+	if gameOver, winner := checkHorizontalWin(1, 2); gameOver {
+		return true, winner, v(0, 2), v(2, 2)
+	}
+	if gameOver, winner := checkHorizontalWin(1, 1); gameOver {
+		return true, winner, v(0, 1), v(2, 1)
+	}
+	if gameOver, winner := checkHorizontalWin(1, 0); gameOver {
+		return true, winner, v(0, 0), v(2, 0)
+	}
 	// check vertical win
-	if gameOver, winner := checkVerticalWin(0, 1); gameOver { return true, winner, v(0,0), v(0,2) }
-	if gameOver, winner := checkVerticalWin(1, 1); gameOver { return true, winner, v(1,0), v(1,2) }
-	if gameOver, winner := checkVerticalWin(2, 1); gameOver { return true, winner, v(2,0), v(2,2) }
+	if gameOver, winner := checkVerticalWin(0, 1); gameOver {
+		return true, winner, v(0, 0), v(0, 2)
+	}
+	if gameOver, winner := checkVerticalWin(1, 1); gameOver {
+		return true, winner, v(1, 0), v(1, 2)
+	}
+	if gameOver, winner := checkVerticalWin(2, 1); gameOver {
+		return true, winner, v(2, 0), v(2, 2)
+	}
 	// check diagonal win
-	if gameOver, winner, v1, v2 := checkDiagonalWin(1, 1); gameOver { return true, winner, v1, v2 }
+	if gameOver, winner, v1, v2 := checkDiagonalWin(1, 1); gameOver {
+		return true, winner, v1, v2
+	}
 	return false, "", zv, zv
 }
 
 func checkHorizontalWin(i, j int) (bool, string) {
 	if state[coordinates[i][j]] == stateEmpty {
 		return false, ""
-	} 
-	if state[coordinates[i][j]] == stateO && 
-	state[coordinates[i-1][j]] == stateO &&
-	state[coordinates[i+1][j]] == stateO {
+	}
+	if state[coordinates[i][j]] == stateO &&
+		state[coordinates[i-1][j]] == stateO &&
+		state[coordinates[i+1][j]] == stateO {
 		return true, "O"
-	} 
-	if state[coordinates[i][j]] == stateX && 
-	state[coordinates[i-1][j]] == stateX &&
-	state[coordinates[i+1][j]] == stateX {
+	}
+	if state[coordinates[i][j]] == stateX &&
+		state[coordinates[i-1][j]] == stateX &&
+		state[coordinates[i+1][j]] == stateX {
 		return true, "X"
 	}
 	return false, ""
@@ -214,15 +228,15 @@ func checkHorizontalWin(i, j int) (bool, string) {
 func checkVerticalWin(i, j int) (bool, string) {
 	if state[coordinates[i][j]] == stateEmpty {
 		return false, ""
-	} 
-	if state[coordinates[i][j]] == stateO && 
-	state[coordinates[i][j-1]] == stateO &&
-	state[coordinates[i][j+1]] == stateO {
+	}
+	if state[coordinates[i][j]] == stateO &&
+		state[coordinates[i][j-1]] == stateO &&
+		state[coordinates[i][j+1]] == stateO {
 		return true, "O"
-	} 
-	if state[coordinates[i][j]] == stateX && 
-	state[coordinates[i][j-1]] == stateX &&
-	state[coordinates[i][j+1]] == stateX {
+	}
+	if state[coordinates[i][j]] == stateX &&
+		state[coordinates[i][j-1]] == stateX &&
+		state[coordinates[i][j+1]] == stateX {
 		return true, "X"
 	}
 	return false, ""
@@ -231,25 +245,25 @@ func checkVerticalWin(i, j int) (bool, string) {
 func checkDiagonalWin(i, j int) (bool, string, vector, vector) {
 	if state[coordinates[i][j]] == stateEmpty {
 		return false, "", zv, zv
-	} 
-	if state[coordinates[i][j]] == stateO && 
-	state[coordinates[i-1][j-1]] == stateO &&
-	state[coordinates[i+1][j+1]] == stateO {
+	}
+	if state[coordinates[i][j]] == stateO &&
+		state[coordinates[i-1][j-1]] == stateO &&
+		state[coordinates[i+1][j+1]] == stateO {
 		return true, "O", v(float64(i-1), float64(j-1)), v(float64(i+1), float64(j+1))
-	} 
-	if state[coordinates[i][j]] == stateO && 
-	state[coordinates[i-1][j+1]] == stateO &&
-	state[coordinates[i+1][j-1]] == stateO {
+	}
+	if state[coordinates[i][j]] == stateO &&
+		state[coordinates[i-1][j+1]] == stateO &&
+		state[coordinates[i+1][j-1]] == stateO {
 		return true, "O", v(float64(i-1), float64(j+1)), v(float64(i+1), float64(j-1))
 	}
-	if state[coordinates[i][j]] == stateX && 
-	state[coordinates[i-1][j-1]] == stateX &&
-	state[coordinates[i+1][j+1]] == stateX {
+	if state[coordinates[i][j]] == stateX &&
+		state[coordinates[i-1][j-1]] == stateX &&
+		state[coordinates[i+1][j+1]] == stateX {
 		return true, "X", v(float64(i-1), float64(j-1)), v(float64(i+1), float64(j+1))
-	} 
-	if state[coordinates[i][j]] == stateX && 
-	state[coordinates[i-1][j+1]] == stateX &&
-	state[coordinates[i+1][j-1]] == stateX {
+	}
+	if state[coordinates[i][j]] == stateX &&
+		state[coordinates[i-1][j+1]] == stateX &&
+		state[coordinates[i+1][j-1]] == stateX {
 		return true, "X", v(float64(i-1), float64(j+1)), v(float64(i+1), float64(j-1))
 	}
 	return false, "", zv, zv
