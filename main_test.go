866d74d8360d0845bff4905bbd88041c1b0ec043
Jarri Abidi <jarri1337@gmail.com>
2020-04-04 12:11:45 +0500

    Add tests for checkDiagonalWin

--- main_test.go
+++ main_test.go
@@ -135,3 +135,65 @@ func TestCheckHorizontalWin(t *testing.T) {
 		})
 	}
 }
+
+func TestCheckDiagonalWin(t *testing.T) {
+	testTable := []testCase{
+		{"O win bottom to top", [][]square{
+			{square{state: o}, square{state: x}, square{state: x}},
+			{square{state: x}, square{state: o}, square{state: x}},
+			{square{state: ""}, square{state: ""}, square{state: o}},
+		}, true, o, square{state: o}, square{state: o}},
+		{"O win top to bottom", [][]square{
+			{square{state: x}, square{state: x}, square{state: o}},
+			{square{state: ""}, square{state: o}, square{state: x}},
+			{square{state: o}, square{state: ""}, square{state: ""}},
+		}, true, o, square{state: o}, square{state: o}},
+		{"No win on O vertical win", [][]square{
+			{square{state: x}, square{state: x}, square{state: ""}},
+			{square{state: ""}, square{state: ""}, square{state: ""}},
+			{square{state: o}, square{state: o}, square{state: o}},
+		}, false, "", square{state: ""}, square{state: ""}},
+		{"X win bottom to top", [][]square{
+			{square{state: x}, square{state: o}, square{state: o}},
+			{square{state: o}, square{state: x}, square{state: ""}},
+			{square{state: ""}, square{state: ""}, square{state: x}},
+		}, true, x, square{state: x}, square{state: x}},
+		{"X win top to bottom", [][]square{
+			{square{state: ""}, square{state: o}, square{state: x}},
+			{square{state: o}, square{state: x}, square{state: ""}},
+			{square{state: x}, square{state: ""}, square{state: o}},
+		}, true, x, square{state: x}, square{state: x}},
+		{"No win on X vertical win", [][]square{
+			{square{state: o}, square{state: o}, square{state: ""}},
+			{square{state: ""}, square{state: ""}, square{state: ""}},
+			{square{state: x}, square{state: x}, square{state: x}},
+		}, false, "", square{state: ""}, square{state: ""}},
+		{"No win on empty", [][]square{
+			{square{state: ""}, square{state: ""}, square{state: ""}},
+			{square{state: ""}, square{state: ""}, square{state: ""}},
+			{square{state: ""}, square{state: ""}, square{state: ""}},
+		}, false, "", square{state: ""}, square{state: ""}},
+		{"No win on tie", [][]square{
+			{square{state: o}, square{state: o}, square{state: x}},
+			{square{state: x}, square{state: x}, square{state: o}},
+			{square{state: o}, square{state: x}, square{state: o}},
+		}, false, "", square{state: ""}, square{state: ""}},
+	}
+	for _, testCase := range testTable {
+		t.Run(testCase.Name, func(t *testing.T) {
+			result, winner, start, end := checkDiagonalWin(testCase.Board)
+			if result != testCase.Result {
+				t.Errorf("Expected result to be %t got %t", testCase.Result, result)
+			}
+			if winner != testCase.Winner {
+				t.Errorf("Expected winner to be %s got %s", testCase.Winner, winner)
+			}
+			if start != testCase.Start {
+				t.Errorf("Expected start square to be %s got %s", testCase.Start.string(), start.string())
+			}
+			if end != testCase.End {
+				t.Errorf("Expected end square to be %s got %s", testCase.End.string(), end.string())
+			}
+		})
+	}
+}
